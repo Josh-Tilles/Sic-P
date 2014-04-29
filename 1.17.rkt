5b0bb168ea77c68a4da6dee2bea3ed10ceef2574
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-04-28 20:47:11 -0400

    fixup! Some trivial cleanups.

--- 1.17.rkt
+++ 1.17.rkt
@@ -8,10 +8,9 @@
 (define (mult a b)
   (cond [(zero? b) 0]
         [(= 1 b)   a]
-        [(odd? b) (+ a 
+        [(odd? b) (+ a
                      (mult a (sub1 b)))]
-        [(even? b) (mult (double a) 
+        [(even? b) (mult (double a)
                          (halve b))]
         [else
          (error "Something is gravely wrong— allegedly the number was neither even nor odd.")]))
-
