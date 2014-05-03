2821dbb8c5109375a2a67ae0650ef5104fd6bd10
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-05-03 10:34:44 -0400

    Solve exercise 1.33.

---
+++ 1.33.rkt
@@ -0,0 +1,35 @@
+#lang racket/base
+(require racket/local
+         (only-in racket/math sqr)
+         (only-in racket/function identity))
+
+;(require ...)
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond [(n . < . (sqr test-divisor)) n]
+        [(divides? test-divisor n) test-divisor]
+        [else (find-divisor n (add1 test-divisor))]))
+(define (divides? a b) (zero? (remainder b a)))
+(define (prime? n)
+  (= n (smallest-divisor n)))
+;; (we needed a `prime?` procedure)
+
+
+(define (filtered-accumulate combiner filter null-value term a next b)
+  (local [(define (iter a result)
+            (if (> a b)
+                result
+                (iter (next a)
+                      (combiner result
+                                (if (filter (term a))
+                                    (term a)
+                                    null-value)))))]
+    (iter a null-value)))
+
+(define (sum-of-squares-of-primes a b)
+  (filtered-accumulate + prime? 0 sqr a add1 b))
+
+(define (product-of-relative-primes n)
+  (define (relatively-prime? i)
+    (= 1 (gcd i n)))
+  (filtered-accumulate * relatively-prime? 1 identity 1 add1 n))
